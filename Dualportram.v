2bfde23fa0eba136af1598e7eefc9a9cadfc902a
Bhavika Govindani <bhavikagovindani66@gmail.com>
2025-07-31 13:23:03 +0530

    Update Dualportram.v

--- Dualportram.v
+++ Dualportram.v
@@ -1,41 +1,38 @@
-module DUAL_PORT_RAM  #(
-    parameter DATA_WIDTH = 8,  
-    parameter ADDR_WIDTH = 4   
-    )
-    (
-    input wire clk,               
-    input wire we_a,               
-    input wire we_b,               
-    input wire [ADDR_WIDTH-1:0] addr_a, 
-    input wire [ADDR_WIDTH-1:0] addr_b, 
-    input wire [DATA_WIDTH-1:0] data_in_a, 
-    input wire [DATA_WIDTH-1:0] data_in_b, 
-    output reg [DATA_WIDTH-1:0] data_out_a, 
-    output reg [DATA_WIDTH-1:0] data_out_b  
-    );
+module dualportram #(
+  parameter ADDR_WIDTH = 4,
+  parameter DATA_WIDTH = 8
+)(
+  input wire                     i_clk,
+  input wire                     i_rst,
+  input wire                     i_valid,
+  input wire                     i_wr_en,
+  input wire                     i_rd_en,
+  input wire                     CS,   //chip select
+  input wire [ADDR_WIDTH-1:0]    i_wraddr,
+  input wire [ADDR_WIDTH-1:0]    i_raddr,
+  input wire [DATA_WIDTH-1:0]    i_wdata,
+  output reg [DATA_WIDTH-1:0]    o_rdata,
+  output reg                     o_ready
+);
 
-    reg [DATA_WIDTH-1:0] ram [2**ADDR_WIDTH-1:0]; 
-    integer i;
-    
-    initial begin
-    for(i=0; i<(1<<ADDR_WIDTH); i=i+1)begin
-    ram[i]=0;
-    end 
-    data_out_a=0;
-    data_out_b=0;
-    end
+  // Memory declaration
+  reg [DATA_WIDTH-1:0] mem [0:(1<<ADDR_WIDTH)-1];
 
-    always @(posedge clk) begin
-        if (we_a) begin
-            ram[addr_a] <= data_in_a;
-        end
-        data_out_a <= ram[addr_a];
+  always @(posedge i_clk or negedge i_rst) begin
+    if (!i_rst) begin
+      o_rdata <= 0;
+      o_ready <= 0;
+    end else begin
+      if (i_valid && CS) begin
+        o_ready <= 1;
+        if (i_wr_en)
+          mem[i_wraddr] <= i_wdata;
+        if (i_rd_en)
+          o_rdata <= mem[i_raddr];
+      end else begin
+        o_ready <= 0;
+      end
     end
+  end
 
-    always @(posedge clk) begin
-        if (we_b) begin
-            ram[addr_b] <= data_in_b; 
-        end
-        data_out_b <= ram[addr_b]; 
-    end
 endmodule
